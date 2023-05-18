d2503c5ff3c3ee763d1a32c302cc83af24666fb6
saorregog <saorregog@gmail.com>
2023-05-18 12:03:26 -0500

    Correccion en linea 8

--- 3. Numero menor en arreglo/NumeroMenorArreglo.js
+++ 3. Numero menor en arreglo/NumeroMenorArreglo.js
@@ -5,7 +5,7 @@ let enunciado = [3, -56, 0, 89, 12];
 
 // FUNCIÓN DE BÚSQUEDA DE NÚMERO MENOR EN UN ARREGLO
 function numeroMenor(arreglo) {
-  let respuesta = enunciado[0]
+  let respuesta = arreglo[0]
   for (let i = 1; i < arreglo.length; i++) {
     if (arreglo[i] < respuesta) {
       respuesta = arreglo[i];
