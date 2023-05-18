cbe09127cabc78382f08fe3c9a6091f8c1832829
saorregog <saorregog@gmail.com>
2023-05-18 12:02:00 -0500

    Version funcional del algoritmo

---
+++ 3. Numero menor en arreglo/NumeroMenorArreglo.js
@@ -0,0 +1,18 @@
+// NÚMERO MENOR EN UN ARREGLO
+
+// DEFINICIÓN DE VARIABLES
+let enunciado = [3, -56, 0, 89, 12];
+
+// FUNCIÓN DE BÚSQUEDA DE NÚMERO MENOR EN UN ARREGLO
+function numeroMenor(arreglo) {
+  let respuesta = enunciado[0]
+  for (let i = 1; i < arreglo.length; i++) {
+    if (arreglo[i] < respuesta) {
+      respuesta = arreglo[i];
+    }
+  }
+  console.log(respuesta);
+}
+
+// BÚSQUEDA DE NÚMERO MENOR EN EL ARREGLO DEL ENUNCIADO
+numeroMenor(enunciado);
